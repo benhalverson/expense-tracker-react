c8766426cab860549f071fa326b9398f1e7d135a
Ben Halverson <benhalverson33@gmail.com>
2021-01-31 02:13:02 -0800

    feat: 🎸 working demo with backend

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,16 +1,24 @@
-import React, {useContext} from 'react';
+import React, { useContext } from 'react';
+import { format } from 'date-fns';
 import { GlobalContext } from '../context/GlobalState';
 export const Transaction = ({ transaction }) => {
 	const sign = transaction.amount < 0 ? '-' : '+';
-	const {deleteTransaction} = useContext(GlobalContext);
+	const { deleteTransaction } = useContext(GlobalContext);
+	const formatter = new Intl.NumberFormat('en-US', {
+		style: 'currency',
+		currency: 'USD'
+	})
 
 	return (
 		<li className={transaction.amount < 0 ? 'minus' : 'plus'}>
 			{transaction.text}{' '}
 			<span>
-				{sign}${Math.abs(transaction.amount)}
-			</span>{' '}
-			<button className="delete-button" onClick={() => deleteTransaction(transaction.id)}>x</button>
+				{formatter.format(transaction.amount)}
+				</span>{' '}
+			<span>Created {format(new Date(transaction.createAt), 'MMM dd')}</span>
+			<button className="delete-button" onClick={() => deleteTransaction(transaction._id)}>
+				x
+			</button>
 		</li>
 	);
 };
